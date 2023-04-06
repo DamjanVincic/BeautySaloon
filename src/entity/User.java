0dfcbc331da18bb6f4773c1080841a0a464cc44f
DamjanVincic <damjanvincic2003@gmail.com>
2023-04-06 20:59:57 +0200

    Added User class

---
+++ src/entity/User.java
@@ -0,0 +1,64 @@
+package entity;
+
+public class User {
+    private String name, surname, gender, phone, address, username, password;
+
+    public User(String name, String surname, String gender, String phone, String address, String username, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+        this.phone = phone;
+        this.address = address;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
