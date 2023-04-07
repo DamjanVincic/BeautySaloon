e3147b7e474fa97685f043da25a5d497753497f6
DamjanVincic <damjanvincic2003@gmail.com>
2023-04-07 13:18:03 +0200

    Changed Employee class

--- src/entity/Employee.java
+++ src/entity/Employee.java
@@ -1,42 +1,43 @@
 package entity;
 
-public class Employee extends User{
-    private String educationLevel;
-    private int workExperience, bonus, salary;
+public abstract class Employee extends User {
+    private EducationLevel educationLevel;
+    private int yearsOfExperience;
+    private double bonus, baseSalary;
 
-    public Employee(String name, String surname, String gender, String phone, String address, String username, String password, String educationLevel, int workExperience, int bonus, int salary) {
+    public Employee(String name, String surname, String gender, String phone, String address, String username, String password, EducationLevel educationLevel, int yearsOfExperience, double bonus, double baseSalary) {
         super(name, surname, gender, phone, address, username, password);
         this.educationLevel = educationLevel;
-        this.workExperience = workExperience;
+        this.yearsOfExperience = yearsOfExperience;
         this.bonus = bonus;
-        this.salary = salary;
+        this.baseSalary = baseSalary;
     }
 
-    public String getEducationLevel() {
+    public EducationLevel getEducationLevel() {
         return this.educationLevel;
     }
-    public void setEducationLevel(String educationLevel) {
+    public void setEducationLevel(EducationLevel educationLevel) {
         this.educationLevel = educationLevel;
     }
 
-    public int getWorkExperience() {
-        return this.workExperience;
+    public int getYearsOfExperience() {
+        return this.yearsOfExperience;
     }
-    public void setWorkExperience(int workExperience) {
-        this.workExperience = workExperience;
+    public void setYearsOfExperience(int yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
     }
 
-    public int getBonus() {
+    public double getBonus() {
         return this.bonus;
     }
-    public void setBonus(int bonus) {
+    public void setBonus(double bonus) {
         this.bonus = bonus;
     }
 
-    public int getSalary() {
-        return this.salary;
+    public double getBaseSalary() {
+        return this.baseSalary;
     }
-    public void setSalary(int salary) {
-        this.salary = salary;
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
     }
 }
