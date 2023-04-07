4ca053692fe32c24f279df2053539b5fa71e54c6
DamjanVincic <damjanvincic2003@gmail.com>
2023-04-07 13:18:19 +0200

    Added Beautician class

---
+++ src/entity/Beautician.java
@@ -0,0 +1,19 @@
+package entity;
+
+import java.util.ArrayList;
+
+public class Beautician extends Employee {
+    private ArrayList<Treatment> treatmentsTrainedFor;
+
+    public Beautician(String name, String surname, String gender, String phone, String address, String username, String password, EducationLevel educationLevel, int workExperience, int bonus, int baseSalary) {
+        super(name, surname, gender, phone, address, username, password, educationLevel, workExperience, bonus, baseSalary);
+        this.treatmentsTrainedFor = new ArrayList<>();
+    }
+
+    public ArrayList<Treatment> getTreatmentsTrainedFor() {
+        return this.treatmentsTrainedFor;
+    }
+    public void setTreatmentsTrainedFor(ArrayList<Treatment> treatmentsTrainedFor) {
+        this.treatmentsTrainedFor = treatmentsTrainedFor;
+    }
+}
