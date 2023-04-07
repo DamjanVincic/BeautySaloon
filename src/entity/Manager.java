ad459ac35fea4646b1badc439bbc16e1d1292432
DamjanVincic <damjanvincic2003@gmail.com>
2023-04-07 13:17:42 +0200

    Added Manager and Receptionist class

---
+++ src/entity/Manager.java
@@ -0,0 +1,7 @@
+package entity;
+
+public class Manager extends Employee {
+    public Manager(String name, String surname, String gender, String phone, String address, String username, String password, EducationLevel educationLevel, int yearsOfExperience, double bonus, double baseSalary) {
+        super(name, surname, gender, phone, address, username, password, educationLevel, yearsOfExperience, bonus, baseSalary);
+    }
+}
