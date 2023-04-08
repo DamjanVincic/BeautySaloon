859df6093f1affa5623c94df53ed12528c4c0e7a
DamjanVincic <damjanvincic2003@gmail.com>
2023-04-08 21:33:57 +0200

    Reverted TreatmentType back to Treatment in Beautician class

--- src/entity/Beautician.java
+++ src/entity/Beautician.java
@@ -3,17 +3,17 @@ package entity;
 import java.util.ArrayList;
 
 public class Beautician extends Employee {
-    private ArrayList<TreatmentType> treatmentTypesTrainedFor;
+    private ArrayList<Treatment> treatmentsTrainedFor;
 
     public Beautician(String name, String surname, String gender, String phone, String address, String username, String password, EducationLevel educationLevel, int workExperience, int bonus, int baseSalary) {
         super(name, surname, gender, phone, address, username, password, educationLevel, workExperience, bonus, baseSalary, EmployeeRole.BEAUTICIAN);
-        this.treatmentTypesTrainedFor = new ArrayList<>();
+        this.treatmentsTrainedFor = new ArrayList<>();
     }
 
-    public ArrayList<TreatmentType> getTreatmentsTrainedFor() {
-        return this.treatmentTypesTrainedFor;
+    public ArrayList<Treatment> getTreatmentsTrainedFor() {
+        return this.treatmentsTrainedFor;
     }
-    public void setTreatmentsTrainedFor(ArrayList<TreatmentType> treatmentTypesTrainedFor) {
-        this.treatmentTypesTrainedFor = treatmentTypesTrainedFor;
+    public void setTreatmentsTrainedFor(ArrayList<Treatment> treatmentsTrainedFor) {
+        this.treatmentsTrainedFor = treatmentsTrainedFor;
     }
 }
