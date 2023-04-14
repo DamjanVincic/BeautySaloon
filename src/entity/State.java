8d29aac230d032d5668cb9285f7dac648addfc91
DamjanVincic <damjanvincic2003@gmail.com>
2023-04-14 19:03:25 +0200

    Reversed State change because of project requirements.

--- src/entity/State.java
+++ src/entity/State.java
@@ -1,9 +1,20 @@
 package entity;
 
 public enum State {
-    SCHEDULED,
-    COMPLETED,
-    CANCELED_CLIENT,
-    CANCELED_SALOON,
-    NOT_SHOWED_UP;
+    SCHEDULED("ZAKAZAN"),
+    COMPLETED("IZVRSEN"),
+    CANCELED_CLIENT("OTKAZAO KLIJENT"),
+    CANCELED_SALOON("OTKAZAO SALON"),
+    NOT_SHOWED_UP("NIJE SE POJAVIO");
+    
+    private final String text;
+
+    State(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return this.text;
+    }
 }
